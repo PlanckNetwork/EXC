3d584d07bbfe69038a233c2bf83a762cc48f82c2
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2025-01-21 19:54:47 -0500

    tr

---
+++ uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/main/go/',
+    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/main/uv/uv.handler.js',
+    bundle: '/main/uv/uv.bundle.js',
+    config: '/main/uv/uv.config.js',
+    sw: '/main/uv/uv.sw.js',
+};
